a92ce5a7729316e302d4a94c9dae108ad57fc21c
Alex_Pevnenko <alex.pevnenko@gmail.com>
2023-05-05 11:36:24 +0300

    Добавил папочную структуру. Добавил maven. Добавил класс для описания логики и тествый класс

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,3 +1,30 @@
-public class Main {
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import lombok.SneakyThrows;
+import javafx.application.Application;
 
+public class Main extends Application{
+
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+    @SneakyThrows
+    public void drawPieChart() {
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Group group = new Group();
+        Scene scene = new Scene(group, 400, 400);
+
+        stage.setTitle("hello world");
+        //добавляем сцену
+        stage.setScene(scene);
+        //отображаем нашу сцену
+        stage.show();
+    }
 }
